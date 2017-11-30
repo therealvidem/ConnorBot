ab5c0c69b488dc608b4a92aae4ef4cc6abba5a4d
therealvidem <superninario@gmail.com>
2017-11-30 08:00:42 -0800

    Update clownfish.js

--- plugins/clownfish.js
+++ plugins/clownfish.js
@@ -1,7 +1,11 @@
 const translate = require('google-translate-api');
+const Enmap = require('enmap');
+const EnmapLevel = require('enmap-level');
+const provider = new EnmapLevel({name: 'clownfish'});
 const events = {};
 const client = require('../main.js').getClient();
 const chance = Math.floor(Math.random() * 50);
+const cooldown = 30 * 1000;
 const flags = {
   'es': '🇪🇸',
   'fr': '🇫🇷',
@@ -9,7 +13,8 @@ const flags = {
   'km': '🇰🇭',
   'zh-cn': '🇨🇳',
   'de': '🇩🇪',
-  'ko': '🇰🇷'
+  'ko': '🇰🇷',
+  'ru': '🇷🇺'
 };
 const languages = [
   'es',
@@ -18,22 +23,33 @@ const languages = [
   'km',
   'zh-cn',
   'de',
-  'ko'
+  'ko',
+  'ru'
 ];
+const usersCooldown = new Set();
 
 function randomFromList(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
 
 events.message = function(msg) {
-  if (msg.content < 1 || msg.author.bot) return;
+  if (msg.content.length < 1 || usersCooldown.has(msg.author.id) || msg.author.bot) return;
   const num = Math.floor(Math.random() * 50);
   const language = randomFromList(languages);
   const flag = flags[language];
+  const id = msg.author.id
   if (num === chance) {
     translate(msg.content, {from: 'en', to: language})
     .then(res => {
       msg.channel.send(`( (${flag}) ${res.text} )`);
+      if (!client.clownfish[id]) {
+        client.clownfish[id] = 0;
+      }
+      client.clownfish[id]++;
+      usersCooldown.add(id);
+      setTimeout(() => {
+        usersCooldown.remove(msg.author.id);
+      }, cooldown);
     })
     .catch(err => {
       console.error(err);
@@ -42,3 +58,18 @@ events.message = function(msg) {
 }
 
 module.exports.events = events;
+module.exports.setup = function() {
+  client.clownfish = new Enmap({provider: provider});
+  client.clownfish.defer.then(() => {
+    if (!client.clownfish) {
+      client.clownfish = {};
+    }
+    console.log('Loaded clownfish data.');
+  });
+}
+module.exports.unload = function(reason) {
+  return new Promise((resolve, reject) => {
+    provider.close();
+    resolve();
+  });
+}
