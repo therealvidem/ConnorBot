92122721eadd7184356f3f0101f44ac5fa6a9ce1
therealvidem <superninario@gmail.com>
2018-04-19 17:59:51 -0700

    Added replace for role group command

--- plugins/common.js
+++ plugins/common.js
@@ -45,13 +45,42 @@ function getRoleAndMember(msg, args) {
   } else {
     query = args.slice(1).join(' ');
   }
-  const role = utils.convertToRole(msg.guild, query)
+  const role = utils.convertToRole(msg.guild, query);
   if (!role) {
     msg.channel.send(`Could not find role "${query}"`);
     return;
   }
   return {
     'role': role,
+    'member': member
+  }
+}
+
+function getRolesAndMember(msg, args) {
+  let query1;
+  let query2;
+  let member = msg.mentions.members.first() || utils.convertToMember(msg.channel, args[0]);
+  // Default to message author if member not found
+  if (!member) {
+    member = msg.member;
+    query1 = args[0];
+    query2 = args[1];
+  } else {
+    query1 = args[1];
+    query2 = args[2];
+  }
+  const role1 = utils.convertToRole(msg.guild, query1);
+  if (!role1) {
+    msg.channel.send(`Could not find role "${query1}"`);
+    return;
+  }
+  const role2 = utils.convertToRole(msg.guild, query2);
+  if (!role2) {
+    msg.channel.send(`Could not find role "${query2}"`);
+    return;
+  }
+  return {
+    'roles': [role1, role2],
     'member': member
   }
 }
@@ -69,51 +98,47 @@ function checkRolePermissions(msg, member, role) {
 }
 
 commands.server = {
-  'get': {
-    'image': function(msg, args) {
-      sendProperty(msg, msg.guild, 'iconURL', 'this guild');
-    },
-    'owner': function(msg, args) {
-      const embed = utils.getUserProfile(msg.guild.owner);
-      msg.channel.send(embed);
-    },
-    'member': function(msg, args) {
-      let member = getMember(msg, args);
-      if (!member) return;
-      const embed = utils.getUserProfile(member);
-      msg.channel.send(embed);
-    }
+  'image': function(msg, args) {
+    sendProperty(msg, msg.guild, 'iconURL', 'this guild');
+  },
+  'owner': function(msg, args) {
+    const embed = utils.getUserProfile(msg.guild.owner);
+    msg.channel.send(embed);
+  },
+  'member': function(msg, args) {
+    let member = getMember(msg, args);
+    if (!member) return;
+    const embed = utils.getUserProfile(member);
+    msg.channel.send(embed);
   }
 }
 
 commands.member = {
-  'get': {
-    'avatar': function(msg, args) {
-      const member = getMember(msg, args);
-      if (!member) return;
-      const user = member.user;
-      sendProperty(msg, user, 'avatarURL', user.tag);
-    },
-    'id': function(msg, args) {
-      const member = getMember(msg, args);
-      if (!member) return;
-      const user = member.user;
-      sendProperty(msg, user, 'id', user.tag);
-    },
-    'username': function(msg, args) {
-      const member = getMember(msg, args);
-      if (!member) return;
-      msg.channel.send(user.tag);
-    },
-    'nickname': function(msg, args) {
-      const member = getMember(msg, args);
-      if (!member) return;
-      const name = member.nickname || member.user.tag;
-      msg.channel.send(name);
-    }
+  'avatar': function(msg, args) {
+    const member = getMember(msg, args);
+    if (!member) return;
+    const user = member.user;
+    sendProperty(msg, user, 'avatarURL', user.tag);
   },
-  'add': {
-    'role': function(msg, args) {
+  'id': function(msg, args) {
+    const member = getMember(msg, args);
+    if (!member) return;
+    const user = member.user;
+    sendProperty(msg, user, 'id', user.tag);
+  },
+  'username': function(msg, args) {
+    const member = getMember(msg, args);
+    if (!member) return;
+    msg.channel.send(user.tag);
+  },
+  'nickname': function(msg, args) {
+    const member = getMember(msg, args);
+    if (!member) return;
+    const name = member.nickname || member.user.tag;
+    msg.channel.send(name);
+  },
+  'role': {
+    'add': function(msg, args) {
       const roleAndMember = getRoleAndMember(msg, args);
       if (!roleAndMember) return;
       const role = roleAndMember.role;
@@ -125,11 +150,11 @@ commands.member = {
       if (!checkRolePermissions(msg, member, role)) return;
       member.addRole(role).then(() => {
         msg.channel.send('Successfully added role');
+      }).catch(() => {
+        msg.channel.send('An error occured');
       });
-    }
-  },
-  'remove': {
-    'role': function(msg, args) {
+    },
+    'remove': function(msg, args) {
       const roleAndMember = getRoleAndMember(msg, args);
       if (!roleAndMember) return;
       const role = roleAndMember.role;
@@ -141,6 +166,29 @@ commands.member = {
       if (!checkRolePermissions(msg, member, role)) return;
       member.removeRole(role).then(() => {
         msg.channel.send('Successfully removed role');
+      }).catch(() => {
+        msg.channel.send('An error occured');
+      });
+    },
+    'replace': function(msg, args) {
+      const rolesAndMember = getRolesAndMember(msg, args);
+      if (!rolesAndMember) return;
+      const role1 = rolesAndMember.roles[0];
+      const role2 = rolesAndMember.roles[1];
+      const member = rolesAndMember.member;
+      if (!member.roles.has(role1.id)) {
+        msg.channel.send(`${member} does not have that role`);
+        return;
+      }
+      if (!checkRolePermissions(msg, member, role1) || !checkRolePermissions(msg, member, role2)) return;
+      member.removeRole(role1).then(() => {
+        member.addRole(role2).then(() => {
+          msg.channel.send('Successfully replaced role');
+        }).catch(() => {
+          msg.channel.send('An error occured');
+        });
+      }).catch(() => {
+        msg.channel.send('An error occured');
       });
     }
   }
