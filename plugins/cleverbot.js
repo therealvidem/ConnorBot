3ef2645bf7843281488fb7bc6693ecdd679322ee
Odeysiuss Tuon <odeysiuss.tuon@wsu.edu>
2018-11-24 23:37:41 -0800

    Reverted cleverbot to having one nick

--- plugins/cleverbot.js
+++ plugins/cleverbot.js
@@ -4,64 +4,35 @@ const cleverbotUrl = 'https://cleverbot.io/1.0/';
 const Enmap = require('enmap');
 const Discord = require('discord.js')
 const client = require('../main.js').getClient();
-const userStates = {};
 const events = {};
 const commands = {};
 
-function init(user, key, nick) {
-  request.post({
-    url: cleverbotUrl + 'create',
-    form: {
-      'user': user,
-      'key': key,
-      'nick': nick
-    }
-  }, (err, httpResponse, body) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const jsonBody = JSON.parse(body);
-    if (!jsonBody.status === 'success') {
-      console.log(jsonBody.status);
-    }
-  });
-}
-
-function reply(msg, channel, id) {
+function reply(msg, channel) {
   const user = client.cleverBot.get('user');
   const key = client.cleverBot.get('key');
-  if (!userStates[id]) {
-    init(user, key, id);
-  }
+  const nick = client.cleverBot.get('nick');
   channel.startTyping();
   request.post({
     url: cleverbotUrl + 'ask',
     form: {
       'user': user,
       'key': key,
-      'nick': id,
+      'nick': nick,
       'text': msg
     }
   }, (err, httpResponse, body) => {
     if (err) {
       channel.send('An error occured');
-      console.log(err);
-      channel.stopTyping();
       return;
     }
     const jsonBody = JSON.parse(body);
     if (jsonBody.status === 'success') {
       channel.send(jsonBody.response);
-      channel.stopTyping();
-      userStates[id] = true;
     } else {
       channel.send('An error occured');
-      console.log(jsonBody.status);
-      channel.stopTyping();
-      return;
     }
   });
+  channel.stopTyping();
 }
 
 events.message = function(msg) {
@@ -75,19 +46,20 @@ events.message = function(msg) {
   Discord.MessageMentions.USERS_PATTERN.lastIndex = 0;
   let cleanContent = msg.cleanContent.trim().split(/ +/g).slice(1);
   if (!test_mention || test_mention.index !== 0 || content.charAt(test_mention.index + test_mention[0].length) !== ' ') return;
-  reply(encodeURI(cleanContent.join(' ')), msg.channel, client.user.id + msg.author.id);
+  reply(encodeURI(cleanContent.join(' ')), msg.channel);
 }
 
 commands.cleverbot = function(msg, args) {
   if (!args[0]) return;
   let cleanContent = msg.cleanContent;
   cleanContent = cleanContent.slice(client.prefix.length + 10); // "c;cleverbot test" -> "test"
-  reply(encodeURI(cleanContent.join(' ')), msg.channel, client.user.id + msg.author.id);
+  reply(encodeURI(cleanContent.join(' ')), msg.channel);
 }
 
 commands.setcleverbot = function(msg, args) {
   let user = args[0];
   let key = args[1];
+  let nick = args[2];
   if (!key || !user || !client.checkOwner(msg)) return;
   if (msg.channel.type != 'dm') {
     msg.channel.send('You can only use that command in DMs');
@@ -95,7 +67,10 @@ commands.setcleverbot = function(msg, args) {
   }
   client.cleverBot.set('user', user);
   client.cleverBot.set('key', key);
-  msg.channel.send(`Successfully set user and key`);
+  if (nick) {
+    client.cleverBot.set('nick', nick);
+  }
+  msg.channel.send(`Successfully set user and key` + (nick ? ' and nick' : ''));
 }
 
 module.exports.events = events;
