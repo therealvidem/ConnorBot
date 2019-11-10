f8473db32a519c8bd5d9388ec4e911873de4cf03
therealvidem <odeysiuss.tuon@wsu.edu>
2019-11-09 21:25:54 -0800

    Very important request by Joanne

--- plugins/common.js
+++ plugins/common.js
@@ -4,12 +4,6 @@ const client = require('../main.js').getClient();
 const utils = require('../utils.js');
 const Discord = require('discord.js');
 
-// events.message = function(msg) {
-//   if (msg.author.id == client.ownerId) {
-//     console.log(`${msg.author.username}#${msg.author.discriminator}: ${msg.cleanContent}`);
-//   }
-// }
-
 commands.ping = function(msg, args) {
   msg.channel.send('Pong!');
 }
