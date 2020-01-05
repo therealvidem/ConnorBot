399434748fc947a7593f51e709ae19eb4b23ffaa
therealvidem <odeysiuss.tuon@wsu.edu>
2020-01-05 04:29:31 -0800

    Added ssml, ipa, and x-sampa support for polly

--- plugins/polly.js
+++ plugins/polly.js
@@ -11,11 +11,14 @@ const Polly = new AWS.Polly({
     signatureVersion: 'v4',
     region: 'us-west-1'
 });
-const cooldown = 20 * 1000;
+const voiceId = 'Miguel';
+const cooldown = 2 * 1000;
+const apostrophePattern = /\w(')\w/;
+const ampersandPattern = /(&)(?![^\s]+;)/;
 const usersCooldown = new Set();
 const commands = {};
 
-function play(msg, stream) {
+function playStream(msg, stream) {
     msg.member.voiceChannel.join().then(connection => {
         client.voiceConnections[msg.guild.id] = {
             'dispatcher': connection.playStream(stream, {
@@ -34,55 +37,119 @@ function play(msg, stream) {
     });
 }
 
+function say(msg, text, type) {
+    
+    const params = {
+        'Text': text,
+        'TextType': type,
+        'OutputFormat': 'ogg_vorbis',
+        'VoiceId': voiceId
+    };
+    
+    Polly.synthesizeSpeech(params, (err, data) => {
+        if (err) {
+            if (err.code === 'InvalidSsmlException') {
+                msg.channel.send('That is invalid SSML code');
+            } else {
+                msg.channel.send('An error occured');
+                console.log(err.code);
+            }
+        } else if (data) {
+            if (data.AudioStream instanceof Buffer) {
+
+                // https://stackoverflow.com/questions/47089230/how-to-convert-buffer-to-stream-in-nodejs
+                let stream = new Stream.Readable({
+                    read() {
+                        this.push(data.AudioStream);
+                        this.push(null);
+                    }
+                });
+                
+                const voiceConnection = client.voiceConnections[msg.guild.id];
+                if (voiceConnection && voiceConnection.dispatcher && !voiceConnection.dispatcher.destroyed) {
+                    voiceConnection.dispatcher.end();
+                    setTimeout(() => {
+                        playStream(msg, stream);
+                    }, 1 * 1000);
+                } else {
+                    playStream(msg, stream);
+                }
+
+                usersCooldown.add(msg.author.id);
+                setTimeout(() => {
+                    usersCooldown.delete(msg.author.id);
+                }, cooldown);
+
+            }
+        }
+    });
+
+}
+
 commands.polly = {
     'say': async function (msg, args) {
         if (!msg.guild) return;
-        const voiceConnection = client.voiceConnections[msg.guild.id];
+        if (msg.member.voiceChannel) {
+            if (usersCooldown.has(msg.author.id)) {
+                msg.channel.send('You must wait before you may use this command again');
+                return;
+            }
+            say(msg, args.join(' '), 'text');
+        } else {
+            msg.channel.send('You must be in a VC to use that command');
+        }
+    },
+    'ssml': async function(msg, args) {
+        if (!msg.guild) return;
         if (msg.member.voiceChannel) {
             if (usersCooldown.has(msg.author.id)) {
                 msg.channel.send('You must wait before you may use this command again');
                 return;
             }
 
-            const params = {
-                'Text': args.join(' '),
-                'OutputFormat': 'ogg_vorbis',
-                'VoiceId': 'Miguel'
-            };
+            const text = args.join(' ')
+                       .replace(apostrophePattern, '&quot;')
+                       .replace(ampersandPattern, '&amp;');
 
-            Polly.synthesizeSpeech(params, (err, data) => {
-                if (err) {
-                    msg.channel.send('An error occured');
-                    console.log(err.code);
-                } else if (data) {
-                    if (data.AudioStream instanceof Buffer) {
-                        // https://stackoverflow.com/questions/47089230/how-to-convert-buffer-to-stream-in-nodejs
-                        let stream = new Stream.Readable({
-                            read() {
-                                this.push(data.AudioStream);
-                                this.push(null);
-                            }
-                        });
-
-                        if (voiceConnection && voiceConnection.dispatcher && !voiceConnection.dispatcher.destroyed) {
-                            voiceConnection.dispatcher.end();
-                            setTimeout(() => {
-                                play(msg, stream);
-                            }, 1 * 1000);
-                        } else {
-                            play(msg, stream);
-                        }
-
-                        usersCooldown.add(msg.author.id);
-                        setTimeout(() => {
-                            usersCooldown.delete(msg.author.id);
-                        }, cooldown);
-
-                    }
-                }
-            });
+            say(msg, text, 'ssml');
         } else {
-            msg.channel.send('You must be in a vc to use that command');
+            msg.channel.send('You must be in a VC to use that command');
+        }
+    },
+    'ipa': async function(msg, args) {
+        if (!msg.guild) return;
+        if (msg.member.voiceChannel) {
+            if (usersCooldown.has(msg.author.id)) {
+                msg.channel.send('You must wait before you may use this command again');
+                return;
+            }
+
+            // https://cuttlesoft.com/blog/pronouncing-things-with-amazons-polly/
+            const text = `<speak><phoneme alphabet='ipa' ph='${args.join(' ').replace('/', '').replace("'", '')}'></phoneme></speak>`;
+
+            console.log(text);
+
+            say(msg, text, 'ssml');
+        } else {
+            msg.channel.send('You must be in a VC to use that command');
+        }
+    },
+    'x-sampa': async function(msg, args) {
+        if (!msg.guild) return;
+        if (msg.member.voiceChannel) {
+            if (usersCooldown.has(msg.author.id)) {
+                msg.channel.send('You must wait before you may use this command again');
+                return;
+            }
+
+            // https://cuttlesoft.com/blog/pronouncing-things-with-amazons-polly/
+            const text = `<speak><phoneme alphabet='x-sampa' ph='${args.join(' ').replace('/', '').replace("'", '')}'></phoneme></speak>`;
+
+            console.log(text);
+
+            say(msg, text, 'ssml');
+        } else {
+            msg.channel.send('You must be in a VC to use that command');
         }
     },
     'stop': async function (msg, args) {
